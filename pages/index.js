27d4ee01aba6b9783d69c0d2ec9002d96ed926ed
Grégory <greg.marini@hotmail.fr>
2023-03-28 17:22:46 +0200

    add data with file

--- pages/index.js
+++ pages/index.js
@@ -1,47 +1,24 @@
 import React, { Fragment } from 'react';
 import FeaturedPosts from '../components/home-page/featured-posts';
 import Hero from '../components/home-page/hero';
+import { getFeaturedPosts } from '../lib/posts-util';
 
-export const DUMMY_POSTS = [
-  {
-    slug: 'getting-started-with-nextjs',
-    title: 'Getting Started with NextJS',
-    image: 'getting-started-nextjs.png',
-    excerpt:
-      'NextJS is the React framework for production - it makes building fullstack React apps and sites  a breeze and ships with built-in SSR',
-    date: '2022-02-10',
-  },
-  {
-    slug: 'getting-started-with-nextjs2',
-    title: 'Getting Started with NextJS',
-    image: 'getting-started-nextjs.png',
-    excerpt:
-      'NextJS is the React framework for production - it makes building fullstack React apps and sites  a breeze and ships with built-in SSR',
-    date: '2022-02-10',
-  },
-  {
-    slug: 'getting-started-with-nextjs3',
-    title: 'Getting Started with NextJS',
-    image: 'getting-started-nextjs.png',
-    excerpt:
-      'NextJS is the React framework for production - it makes building fullstack React apps and sites  a breeze and ships with built-in SSR',
-    date: '2022-02-10',
-  },
-  {
-    slug: 'getting-started-with-nextjs4',
-    title: 'Getting Started with NextJS',
-    image: 'getting-started-nextjs.png',
-    excerpt:
-      'NextJS is the React framework for production - it makes building fullstack React apps and sites  a breeze and ships with built-in SSR',
-    date: '2022-02-10',
-  },
-];
-
-export default function HomePage() {
+export default function HomePage({ posts }) {
   return (
     <Fragment>
       <Hero />
-      <FeaturedPosts posts={DUMMY_POSTS} />
+      <FeaturedPosts posts={posts} />
     </Fragment>
   );
+}
+
+export function getStaticProps() {
+  const featuredPosts = getFeaturedPosts();
+
+  return {
+    props: {
+      posts: featuredPosts,
+    },
+    revalidate: 3600,
+  };
 }
--- pages/posts/index.js
+++ pages/posts/index.js
@@ -3,5 +3,5 @@ import { DUMMY_POSTS } from '..';
 import AllPosts from '../../components/posts/all-posts';
 
 export default function AllPostsPage() {
-  return <AllPosts posts={DUMMY_POSTS} />;
+  return <div>rien</div>;
 }
