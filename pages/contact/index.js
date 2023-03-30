37819afc12e758d0628e8b7b416b9e11c0e8f198
Grégory Marini <greg.marini@hotmail.fr>
2023-03-30 11:22:44 +0200

    add form with style

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -1,7 +1,6 @@
-import React from 'react'
+import React from 'react';
+import ContactForm from '../../components/contact/contact-form';
 
 export default function ContactPage() {
-  return (
-    <div>Contact</div>
-  )
+  return <ContactForm />;
 }
