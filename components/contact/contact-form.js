38078a3cb0393f25bf8cfe77b8a4b28ea0c9faf2
Grégory Marini <greg.marini@hotmail.fr>
2023-03-30 15:59:08 +0200

    add notification

--- components/contact/contact-form.js
+++ components/contact/contact-form.js
@@ -1,21 +1,53 @@
-import { useState } from 'react';
+import { use, useEffect, useState } from 'react';
+
 import styles from './contact-form.module.scss';
+import Notification from '../ui/notification';
+
+async function sendcontactData(contactDetails) {
+  const response = await fetch('/api/contact', {
+    method: 'POST',
+    body: JSON.stringify(contactDetails),
+    headers: { 'Content-Type': 'application/json' },
+  });
+
+  const data = await response.json();
+
+  if (!response.ok) {
+    throw new Error(data.message || 'Something went wrong!');
+  }
+}
 
 export default function ContactForm() {
   const initialContactData = { email: '', name: '', message: '' };
   const [contactData, setContactData] = useState(initialContactData);
+  const [requestStatus, setRequestStatus] = useState(); // 'pending', 'success', 'error'
+  const [requestError, setRequestError] = useState();
+
   const { email, name, message } = contactData;
 
-  function sendMessageHandler(e) {
+  useEffect(() => {
+    if (requestStatus === 'success' || requestStatus === 'error') {
+      const timer = setTimeout(() => {
+        setRequestStatus(null);
+        setRequestError(null);
+      }, 3000);
+
+      return () => clearTimeout(timer);
+    }
+  }, [requestStatus]);
+
+  async function sendMessageHandler(e) {
     e.preventDefault();
 
-    console.log(contactData);
+    setRequestStatus('pending');
 
-    fetch('/api/contact', {
-      method: 'POST',
-      body: JSON.stringify(contactData),
-      headers: { 'Content-Type': 'application/json' },
-    });
+    try {
+      await sendcontactData(contactData);
+      setRequestStatus('success');
+    } catch (error) {
+      setRequestError(error.message);
+      setRequestStatus('error');
+    }
 
     setContactData(initialContactData);
   }
@@ -27,6 +59,32 @@ export default function ContactForm() {
       [name]: value,
     }));
   };
+
+  let notification;
+
+  if (requestStatus === 'pending') {
+    notification = {
+      status: 'pending',
+      title: 'Sending Message...',
+      message: 'Your message is on its way!',
+    };
+  }
+
+  if (requestStatus === 'success') {
+    notification = {
+      status: 'success',
+      title: 'Success!',
+      message: 'Message sent successfully!',
+    };
+  }
+
+  if (requestStatus === 'error') {
+    notification = {
+      status: 'error',
+      title: 'Error!',
+      message: requestError,
+    };
+  }
 
   return (
     <section className={styles.contact}>
@@ -76,6 +134,13 @@ export default function ContactForm() {
           <button>Send Message</button>
         </div>
       </form>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
     </section>
   );
 }
--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,31 +1,53 @@
-export default function handler(req, res) {
-  if (req.method === 'POST') {
-    const { email, name, message } = req.body;
+import { MongoClient } from 'mongodb';
+require('dotenv').config();
 
-    if (
-      !email ||
-      !email.includes('@') ||
-      !email.includes('.') ||
-      !name ||
-      name.trim() === '' ||
-      !message ||
-      message.trim() === ''
-    ) {
-      res.status(422).json({ message: 'Invalid input.' });
+export async function connectDatabase() {
+  return await MongoClient.connect(process.env.MONGO_DB_URL);
+}
+
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
+    try {
+      const { email, name, message } = req.body;
+
+      if (
+        !email ||
+        !email.includes('@') ||
+        !email.includes('.') ||
+        !name ||
+        name.trim() === '' ||
+        !message ||
+        message.trim() === ''
+      ) {
+        res.status(422).json({ message: 'Invalid input.' });
+        return;
+      }
+
+      // Store it in a database
+      const newMessage = {
+        email,
+        name,
+        message,
+      };
+
+      let client;
+
+      client = await connectDatabase();
+
+      const db = client.db();
+
+      const result = await db.collection('messages').insertOne(newMessage);
+
+      client.close();
+
+      console.log(result);
+
+      res
+        .status(201)
+        .json({ message: 'Successfully stored message!', message: newMessage });
+    } catch (error) {
+      res.status(500).json({ message: error.message });
       return;
     }
-
-    // Store it in a database
-    const newMessage = {
-      email,
-      name,
-      message,
-    };
-
-    console.log(newMessage);
-
-    res
-      .status(201)
-      .json({ message: 'Successfully stored message!', message: newMessage });
   }
 }
